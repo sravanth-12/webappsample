3efecd4cdcb77a084a582ce41e52d025a5d6e89d
Rune <rs@appharbor.com>
2013-05-06 16:28:38 -0700

    Initial commit

---
+++ WebAppSample/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+
+namespace WebAppSample
+{
+	public class Global : HttpApplication
+	{
+
+		void Application_Start(object sender, EventArgs e)
+		{
+		}
+
+		void Application_End(object sender, EventArgs e)
+		{
+		}
+
+		void Application_Error(object sender, EventArgs e)
+		{
+		}
+
+		void Session_Start(object sender, EventArgs e)
+		{
+		}
+
+		void Session_End(object sender, EventArgs e)
+		{
+		}
+
+	}
+}
